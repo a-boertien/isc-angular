78f9d673d05737b7fbfbef8e9e89d74da0fe7c17
John Paxton <pax@speedingplanet.com>
2020-02-13 14:36:24 -0500

    PayeesList sorting with Observables

--- src/app/payees/payees-list/payees-list-observable.component.ts
+++ src/app/payees/payees-list/payees-list-observable.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Payee } from '..';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { tap, map, startWith } from 'rxjs/operators';
+import * as lodash from 'lodash';
+
+type SortDirection = 'asc' | 'desc';
+
+interface SortConfig {
+  sortField: string;
+  sortDirection: SortDirection;
+}
 
 @Component({
   selector: 'payees-list-observable',
   templateUrl: './payees-list-observable.component.html',
-  styles: []
+  styles: [
+    `
+      thead th {
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PayeesListObservableComponent implements OnInit {
   @Input()
@@ -14,14 +28,58 @@ export class PayeesListObservableComponent implements OnInit {
   displayPayees: Observable<Payee[]>;
   loaded = false;
 
+  sortConfig: SortConfig = {
+    sortDirection: 'asc',
+    sortField: ''
+  };
+
+  sortSubject = new Subject<SortConfig>();
+
   constructor() {}
 
   ngOnInit(): void {
-    this.displayPayees = this.payees.pipe(
+    const sortObservable = this.sortSubject
+      .asObservable()
+      .pipe(startWith(this.sortConfig));
+    const mergedObservable = combineLatest([sortObservable, this.payees]);
+
+    this.displayPayees = mergedObservable.pipe(
       tap(() => {
         console.log('Tapped');
         this.loaded = true;
+      }),
+      map(([sortConfig, payees]) => {
+        return lodash.orderBy(
+          payees,
+          sortConfig.sortField,
+          sortConfig.sortDirection
+        );
+
+        // Plain JS (without an actual sort() routine)
+        /*
+        const sortedPayees = [...payees];
+        sortedPayees.sort((a, b) => 1);
+        return this.sortConfig.sortDirection === 'asc'
+          ? sortedPayees
+          : sortedPayees.reverse();
+          */
       })
     );
+  }
+
+  handleHeaderClick(sortField: string) {
+    console.log(`Sorting on ${sortField}`);
+    let sortDirection: SortDirection = 'asc';
+
+    if (
+      sortField === this.sortConfig.sortField &&
+      this.sortConfig.sortDirection === 'asc'
+    ) {
+      sortDirection = 'desc';
+    }
+
+    this.sortConfig = { sortField, sortDirection };
+
+    this.sortSubject.next(this.sortConfig);
   }
 }
